e8b252ea54ec890e8a5b4326a8ef1ced81634a05
Ryan Chen <xryanchen@gmail.com>
2018-02-16 00:09:32 -0800

    Add todo list example

---
+++ demo/TodoApp/test.conf.js
@@ -0,0 +1,13 @@
+const unitTestConf = {
+  testFilePattern: 'src/**/*.test.js',
+  useES6: true
+}
+
+const functionalTestConf = {
+
+}
+
+module.exports = {
+  unitTest: unitTestConf,
+  functionalTest: functionalTestConf
+};
---
+++ test.conf.default.js
@@ -0,0 +1,13 @@
+const unitTestConf = {
+  testFilePattern: '**/*.test.js',
+  useES6: false
+}
+
+const functionalTestConf = {
+
+}
+
+module.exports = {
+  unitTest: unitTestConf,
+  functionalTest: functionalTestConf
+};
